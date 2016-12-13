a0dfb13ca1f14b20108cafa87a822965200fdbd1
iskandar <iskandar@askarov.org>
2016-12-12 20:10:58 -0500

    -initial commit

---
+++ t1/l2/color_selection.py
@@ -0,0 +1,34 @@
+'''
+Created on Dec 12, 2016
+
+@author: iskandar
+'''
+
+import matplotlib.pyplot as plt;
+import matplotlib.image as mpimg;
+
+import numpy as np;
+
+
+image = mpimg.imread('../../resources/images/test.jpg');
+
+print("Type:", type(image), ' dimensions: ', image.shape);
+
+
+ysize = image.shape[0];
+xsize = image.shape[1];
+
+color_select = np.copy(image);
+
+red_threshold = 0x0;
+green_threshold = 0x0;
+blue_threshold = 0x0;
+
+rgb_thld = [red_threshold, green_threshold, blue_threshold];
+
+threshold = (image[:,:,0] < rgb_thld[0]) | (image[:,:,1] < rgb_thld[1]) | (image[:,:,2] < rgb_thld[2]);
+color_select[threshold] = [0, 0, 0];
+plt.imshow(color_select); 
+
+plt.imshow(color_select); 
+print()
