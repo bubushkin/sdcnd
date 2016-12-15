7689787e3e8c18672df84945ab82d2aa9a294d5c
iskandar <iskandar@izzi>
2016-12-14 20:47:03 -0500

    -implemented region select along with color selection.

--- t1/l2/color_region_selection.py
+++ t1/l2/color_region_selection.py
@@ -1,14 +1,14 @@
 '''
-Created on Dec 12, 2016
+Created on Dec 14, 2016
 
 @author: iskandar
 '''
+
 
 import matplotlib.pyplot as plt;
 import matplotlib.image as mpimg;
 
 import numpy as np;
-
 
 image = mpimg.imread('../../resources/images/test.jpg');
 
@@ -18,7 +18,15 @@ print("Type:", type(image), ' dimensions: ', image.shape);
 ysize = image.shape[0];
 xsize = image.shape[1];
 
-region_select = np.copy(image);
+color_select = np.copy(image);
+line_image = np.copy(image);
+
+red_threshold = 200;
+green_threshold = 200;
+blue_threshold = 200;
+
+rgb_threshold = [red_threshold, green_threshold, blue_threshold];
+
 
 left_bottom = [0, 539];
 right_bottom = [960, 539];
@@ -28,12 +36,21 @@ fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1);
 fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1);
 fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1);
 
+color_threshold = (image[:,:,0] < rgb_threshold[0]) | (image[:,:,1] < rgb_threshold[1]) | (image[:,:,2] < rgb_threshold[2]);
+
+
 XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize));
 
 region_threshold = (YY > (XX*fit_left[0] + fit_left[1])) & (YY > (XX*fit_right[0] + fit_right[1])) & (YY < (XX*fit_bottom[0] + fit_bottom[1]));
 
-region_select[region_threshold] = [255, 0, 0];
 
-plt.imshow(region_select);
+color_select[color_threshold] = [0, 0, 0];
+
+line_image[~color_threshold & region_threshold] = [255, 0, 0];
+
+plt.imshow(color_select);
+plt.imshow(line_image);
+
 print();
- 
+
+
